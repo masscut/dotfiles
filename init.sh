d3e79ec273c7aad943549b68e04fe3284f60e302
Masaki Kato <masscut@gmail.com>
2012-02-02 19:36:59 +0900

    modified init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #/bin/sh
+PWD=`pwd`
 
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim.git
 
-ln -s .vimrc ~/.vimrc
-ln -s .vim ~/.vim
-ln -s .screenrc ~/.screenrc
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.vim ~/.vim
+ln -s $PWD/.screenrc ~/.screenrc
