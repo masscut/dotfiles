52ff2511a9d77a75637ae5bcb207cfcfc54012ae
Masaki Kato <masscut@gmail.com>
2012-07-07 20:12:18 +0900

    add .bashrc and forcus on ubuntu

--- init.sh
+++ init.sh
@@ -1,15 +1,12 @@
 #/bin/sh
 PWD=`pwd`
 
-# Centos
-redhat=/etc/redhat-release
-if [ -e $redhat ]; then
-  sudo yum -y install vim-enhanced
-fi
-
 git submodule init
 git submodule update
 
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
 ln -s $PWD/.screenrc ~/.screenrc
+ln -s $PWD/.bashrc ~/.bashrc
+
+vi +NeoBundleInstall +q
