8ac8ddf3a7e04b2aea3371a4ade311087afaf828
Masa Kato <masscut@gmail.com>
2015-05-02 20:14:51 +0900

    Instalation neobundle is in init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
 #/bin/sh
 PWD=`pwd`
 
+# Delete existing dotfiles
 rm -f ~/.vimrc
 rm -f ~/.gvimrc
 rm -rf ~/.vim
 rm -f ~/.bashrc
 rm -f ~/.zshrc
 
+# Create symlink
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.gvimrc ~/.gvimrc
 ln -s $PWD/.vim ~/.vim
@@ -14,4 +16,5 @@ ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.berkshelf ~/.berkshelf
 
-#vi +NeoBundleInstall +q
+# Install neobundle.vim
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
