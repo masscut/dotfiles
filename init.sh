2a5b114bfa5b05048b9b1436fc38c24a4b75a041
Masa Kato <masscut@gmail.com>
2016-05-30 19:38:35 +0900

    remove .

--- init.sh
+++ init.sh
@@ -8,11 +8,7 @@ rm -rf ~/.vim
 rm -f ~/.zshrc
 
 # Create symlink
-ln -s $PWD/.vimrc ~/.vimrc
-ln -s $PWD/.gvimrc ~/.gvimrc
-ln -s $PWD/.vim ~/.vim
-ln -s $PWD/.zshrc ~/.zshrc
-ln -s $PWD/.berkshelf ~/.berkshelf
-
-# Install neobundle.vim
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/gvimrc ~/.gvimrc
+ln -s $PWD/vim ~/.vim
+ln -s $PWD/zshrc ~/.zshrc
