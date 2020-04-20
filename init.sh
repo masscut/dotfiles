cd463a0393e7cff081c971cbc646945e7b65cb76
Masa Kato <mskt4440@gmail.com>
2020-04-20 20:06:25 +0900

    removed unneccesary packages

--- init.sh
+++ init.sh
@@ -10,12 +10,12 @@ rm -rf ~/.tmux
 
 # Create symlink
 ln -sf $PWD/vimrc ~/.vimrc
-ln -sf $PWD/gvimrc ~/.gvimrc
+#ln -sf $PWD/gvimrc ~/.gvimrc
 ln -sf $PWD/vim ~/.vim
-ln -sf $PWD/tmux.conf ~/.tmux.conf
-ln -sfs $PWD/tmux ~/.tmux
+#ln -sf $PWD/tmux.conf ~/.tmux.conf
+#ln -sfs $PWD/tmux ~/.tmux
 ln -sf $PWD/fish/config.fish  ~/.config/fish/config.fish
 ln -sf $PWD/fish/fishfile  ~/.config/fish/fishfile
-ln -sf $PWD/fish/conf.d/direnv.fish  ~/.config/fish/conf.d/direnv.fish
+#ln -sf $PWD/fish/conf.d/direnv.fish  ~/.config/fish/conf.d/direnv.fish
 ln -sf $PWD/fish/conf.d/peco.fish  ~/.config/fish/conf.d/peco.fish
 ln -sf $PWD/peco  ~/.config/peco
