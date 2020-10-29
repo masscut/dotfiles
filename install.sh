ea5366b963d08bec0898426db596434da9ab4968
Masa Kato <mskt4440@gmail.com>
2020-10-29 11:24:44 +0900

    removed unnecessary files

---
+++ install.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+PWD=`pwd`
+
+# Delete existing dotfiles
+# rm -f ~/.vimrc
+# rm -f ~/.gvimrc
+# rm -rf ~/.vim
+
+# install fisher
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+# Create symlink
+# ln -sf $PWD/vimrc ~/.vimrc
+# ln -sf $PWD/vim ~/.vim
+ln -sf $PWD/fish/config.fish  ~/.config/fish/config.fish
+ln -sf $PWD/fish/fishfile  ~/.config/fish/fishfile
+ln -sf $PWD/fish/conf.d/peco.fish  ~/.config/fish/conf.d/peco.fish
+ln -sf $PWD/peco  ~/.config/peco
+
